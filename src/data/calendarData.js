0a6cd5636b61d3b4625f86cb80004dce12c0e5fc
A3Tom <Tom_Esq@Yahoo.com>
2023-03-26 22:56:34 +0100

    basic dummy data commit

---
+++ src/data/calendarData.js
@@ -0,0 +1,169 @@
+export default [
+	{
+		dayIdx: 0,
+		dayName: "Montag",
+		dayBlocks: [
+			{
+				title: "Working hours",
+				startHrs: 2,
+				blockType: 3,
+				duration: 7.5,
+			},
+			{
+				title: "Core hours",
+				startHrs: 4,
+				blockType: 1,
+				duration: 4,
+			},
+			{
+				title: "break",
+				blockType: 2,
+				startHrs: 3.25,
+				duration: 0.25,
+			},
+			{
+				title: "Lunch",
+				blockType: 2,
+				startHrs: 6,
+				duration: 0.75,
+			},
+		],
+	},
+	{
+		dayIdx: 1,
+		dayName: "Dienstag",
+		dayBlocks: [
+			{
+				title: "Working hours",
+				startHrs: 2,
+				blockType: 3,
+				duration: 7.5,
+			},
+			{
+				title: "Core hours",
+				startHrs: 4,
+				blockType: 1,
+				duration: 4,
+			},
+			{
+				title: "break",
+				blockType: 2,
+				startHrs: 3.25,
+				duration: 0.25,
+			},
+			{
+				title: "Lunch",
+				blockType: 2,
+				startHrs: 6,
+				duration: 0.75,
+			},
+		],
+	},
+	{
+		dayIdx: 2,
+		dayName: "Mittwoch",
+		dayBlocks: [
+			{
+				title: "Working hours",
+				startHrs: 2,
+				blockType: 3,
+				duration: 7.5,
+			},
+			{
+				title: "Core hours",
+				startHrs: 4,
+				blockType: 1,
+				duration: 4,
+			},
+			{
+				title: "break",
+				blockType: 2,
+				startHrs: 3.25,
+				duration: 0.25,
+			},
+			{
+				title: "Lunch",
+				blockType: 2,
+				startHrs: 6,
+				duration: 0.75,
+			},
+		],
+	},
+	{
+		dayIdx: 3,
+		dayName: "Donnerstag",
+		dayBlocks: [
+			{
+				title: "Working hours",
+				startHrs: 2.75,
+				blockType: 3,
+				duration: 4.75,
+			},
+			{
+				title: "Working hours",
+				startHrs: 9,
+				blockType: 3,
+				duration: 1.25,
+			},
+			{
+				title: "Core hours",
+				startHrs: 4,
+				blockType: 1,
+				duration: 4,
+			},
+			{
+				title: "Lunch",
+				blockType: 2,
+				startHrs: 6,
+				duration: 0.5,
+			},
+		],
+	},
+	{
+		dayIdx: 4,
+		dayName: "Freitag",
+		dayBlocks: [
+			{
+				title: "Working hours",
+				startHrs: 3,
+				blockType: 3,
+				duration: 5,
+			},
+			{
+				title: "Core hours",
+				startHrs: 4,
+				blockType: 1,
+				duration: 4,
+			},
+			{
+				title: "break",
+				blockType: 2,
+				startHrs: 4.25,
+				duration: 0.25,
+			},
+			{
+				title: "Lunch",
+				blockType: 2,
+				startHrs: 5.5,
+				duration: 0.5,
+			},
+		],
+	},
+	{
+		dayIdx: 5,
+		dayName: "Samstag",
+		dayBlocks: [
+			{
+				title: "Working hours",
+				startHrs: 5,
+				blockType: 3,
+				duration: 1.25,
+			},
+		],
+	},
+	{
+		dayIdx: 6,
+		dayName: "Sonntag",
+		dayBlocks: [],
+	},
+];
