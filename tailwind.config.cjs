a694ddc1c1c17d53c9a80e5f5f942f2bb0f37395
A3Tom <Tom_Esq@Yahoo.com>
2023-03-26 22:56:04 +0100

    Got a nice wee font on the go

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+    fontFamily: {
+      "primary": "Montserrat, sans-serif",
+    },
+		extend: {
+		},
 	},
 	plugins: [],
 };
